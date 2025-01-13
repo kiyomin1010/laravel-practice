81402427f9e413758ec6ee75e4afe8851ebd946f
kiyomin0615 <moeday1010@gmail.com>
2025-01-13 11:57:17 +0900

    01-send post request with a CSRF token

--- 01-task-list/routes/web.php
+++ 01-task-list/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,17 +10,26 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     $tasks = Task::latest()->get();
+
     return view('index', [
         'tasks' => $tasks,
     ]);
 })->name('tasks.index'); // 'index' means list of resources
 
+Route::view('tasks/create', 'create')
+    ->name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id) {
     $task = Task::findOrFail($id);
+
     return view('show', [
         'task' => $task,
     ]);
 })->name('tasks.show'); // 'show' means details of the resource
+
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('tasks.store');
 
 Route::fallback(function () {
     return 'Could not find any resource for this route';
