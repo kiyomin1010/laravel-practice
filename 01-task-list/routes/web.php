37094cfd7445bb521eae335324185436699eb607
kiyomin0615 <moeday1010@gmail.com>
2025-01-13 14:42:02 +0900

    01-edit task with PUT request and restore old values with old() function

--- 01-task-list/routes/web.php
+++ 01-task-list/routes/web.php
@@ -27,6 +27,14 @@ Route::get('/tasks/{id}', function ($id) {
     ]);
 })->name('tasks.show'); // 'show' means details of the resource
 
+Route::get('/tasks/{id}/edit', function ($id) {
+    $task = Task::findOrFail($id);
+
+    return view('edit', [
+        'task' => $task,
+    ]);
+})->name('tasks.edit'); // 'show' means details of the resource
+
 Route::post('/tasks', function (Request $request) {
     // If validation fails, the error and flash messages are stored in the user session
     $data = $request->validate([
@@ -45,6 +53,25 @@ Route::post('/tasks', function (Request $request) {
     return redirect()->route('tasks.show', ['id' => $task->id])
         ->with('success', 'New task created successfully!');
 })->name('tasks.store');
+
+Route::put('/tasks/{id}', function ($id, Request $request) {
+    // If validation fails, the error and flash messages are stored in the user session
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
 
 Route::fallback(function () {
     return 'Could not find any resource for this route';
