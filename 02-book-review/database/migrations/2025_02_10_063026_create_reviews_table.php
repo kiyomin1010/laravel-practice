c958bdbdb5690ed9372ea1ed8b2dd36f69693382
kiyomin0615 <moeday1010@gmail.com>
2025-02-10 16:36:38 +0900

    02-add one-to-many relationship between Book and Review

--- 02-book-review/database/migrations/2025_02_10_063026_create_reviews_table.php
+++ 02-book-review/database/migrations/2025_02_10_063026_create_reviews_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+
+            // $table->unsignedBigInteger('book_id');
+            // $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
+
             $table->text('review');
             $table->unsignedTinyInteger('rating');
             $table->timestamps();
